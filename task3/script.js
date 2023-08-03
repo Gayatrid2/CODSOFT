a3f94d428c1481bdad3c9ebca0ea971fbf614d3d
Gayatrid2 <gayatrideore2075@gmail.com>
2023-08-03 13:22:17 +0530

    each btn is being selected sucessfully through script

--- task3/script.js
+++ task3/script.js
@@ -3,6 +3,9 @@ const digits = document.querySelectorAll(".cal-digit-val p");
 const operatorsDiv = document.querySelectorAll(".cal-operator div");
 const digitsDiv = document.querySelectorAll(".cal-digit-val");
 
+const equalDiv = document.querySelector(".cal-equal");
+const equal = document.querySelector(".cal-equal-val");
+
 const ans = null;
 
 // adding on press event shadow null event to each btn
@@ -26,6 +29,16 @@ digitsDiv?.forEach((operator, index) => {
   };
 });
 
+equalDiv.onclick = (event) => {
+  equalDiv.style.boxShadow = "none";
+  setTimeout(() => {
+    equalDiv.style.boxShadow =
+      "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px";
+  }, 100);
+};
+
+// operation on click
+
 operators?.forEach((operator, index) => {
   operator.onclick = (event) => {
     console.log(operator);
@@ -37,3 +50,7 @@ digits?.forEach((digit, index) => {
     console.log(digit);
   };
 });
+
+equalDiv.onclick = (event) => {
+  console.log(equal);
+};
